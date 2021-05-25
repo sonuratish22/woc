5a4db46716c6d7b839bc4cb6001dc3fb852ff398
sonu@2202 <sonu.kumar@aristocrat.com>
2021-05-25 22:40:32 +0530

    adding a new method to check

---
+++ src/main/java/teststeps/ABC.java
@@ -0,0 +1,16 @@
+package teststeps;
+
+import org.testng.annotations.Test;
+
+public class ABC {
+    @Test
+    public void mTest() {
+        System.out.println("hello ");
+    }
+
+    public void nTest() {
+        System.out.println("hello n Test");
+    }
+
+
+}
--- src/main/java/teststeps/Test.java
+++
@@ -1,8 +0,0 @@
-package teststeps;
-
-public class Test {
-
-    public static void main(String[] args) {
-        System.out.println("hello ");
-    }
-}
